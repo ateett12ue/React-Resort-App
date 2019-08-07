cb01cf0dc0f18eb361eac875d71191e62bf71207
ateet <16ucs050@lnmiit.ac.in>
2019-08-07 23:55:12 +0530

    React router dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/HomeComponent";
 import Rooms from "./pages/RoomsComponent";
 import Error from "./pages/errorComponent";
 import SingleRoom from "./pages/singleRoomComponent";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
