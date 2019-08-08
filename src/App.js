c7455908891f8de124a12cefed7503ca7e1a6184
ateet <16ucs050@lnmiit.ac.in>
2019-08-08 11:35:58 +0530

    Hero setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
-import Error from "./pages/error";
+import Error from "./pages/Error";
 import SingleRoom from "./pages/singleRoom";
 import Navbar from "./components/Navbar";
 import { Route, Switch } from "react-router-dom";
