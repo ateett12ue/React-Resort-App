52ed0f0621170b100930235cc7468d22a825ae3a
ateet <16ucs050@lnmiit.ac.in>
2019-08-09 23:53:38 +0530

    Final update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero>
         <Banner title="luxurious rooms" subtitle="delux rooms starting at $299">
           <Link to="/rooms" className="btn-primary">
-            Our room
+            Our rooms
           </Link>
         </Banner>
       </Hero>
