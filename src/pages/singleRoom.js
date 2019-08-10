d72bc687996ee97612155f4c9dd0abdfd4b1ba9a
ateet <16ucs050@lnmiit.ac.in>
2019-08-10 23:07:17 +0530

    Single room page, excesing single room property error

--- src/pages/singleRoom.js
+++ src/pages/singleRoom.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    };
+  }
+  static contextType = RoomContext;
+  //componentDidMount() {}
   render() {
-    return <div>hello from single room</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+    return <div>Hello {room.name}</div>;
   }
 }
