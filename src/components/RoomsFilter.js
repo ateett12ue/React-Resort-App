4fe51f1fd6a77faaa1ec13228d572323b4ba7905
ateet <16ucs050@lnmiit.ac.in>
2019-08-11 22:39:11 +0530

    first filter set with context value and unique value setup

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,5 +1,58 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
-export default function RoomsFilter() {
-  return <div>hello from the room filter</div>;
+//get all unique value
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+export default function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+  //get uniqe types
+  let types = getUnique(rooms, "type");
+  //add all
+  types = ["all", ...types];
+  //map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/*Select Type*/}
+        <div className="form-group">
+          <label htmlfor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/*End Of Select Type*/}
+      </form>
+    </section>
+  );
 }
--- src/context.js
+++ src/context.js
@@ -56,7 +56,9 @@ class RoomProvider extends Component {
     const type = event.target.type;
     const name = event.target.name;
     const value = event.target.value;
-    console.log(type, name, value);
+    console.log(
+      `this is type: ${type}, this is name: ${name}, this is value: ${value}`
+    );
   };
 
   filterRooms = () => {
